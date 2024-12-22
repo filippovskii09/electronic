d666eda9b5c5b3668115bef0a4290419cfd4c414
artur.filippovskii <arturfilippovskij@gmail.com>
2024-12-22 10:22:37 +0200

    add router setup

---
+++ src/pages/pagesData.tsx
@@ -0,0 +1,12 @@
+import {routerType} from "../types/router.types";
+import HomeView from "../views/HomeView";
+
+const pagesData: routerType[] = [
+  {
+    path: "/",
+    element: <HomeView />,
+    title: "home"
+  }
+]
+
+export default pagesData;
---
+++ src/views/HomeView.tsx
@@ -0,0 +1,7 @@
+const HomeView = () => {
+  return (
+    <h1>Home Page</h1>
+  );
+}
+
+export default HomeView;
